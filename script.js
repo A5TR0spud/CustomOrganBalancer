1cdd153b8d5ed89ed733aecb707a3c4e78696b78
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2024-04-26 10:04:11 -0400

    fix icon png, comment js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 document.write("<p>Hello World!</p>");
+
+//todo: take organ_scores.json, create a flex list with name : input score : * weight,
+//hoverable tooltip, final score calculator, verdict "translator"
+///give command, json generator
