3c9de9f1f36686d75c29278fc45859d95ac3e21e
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2024-04-30 11:48:38 -0400

    hoverable tooltip
    been here like a thousand times

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ async function createList() {
     let x = await fetch("./data/organ_scores.json");
     let jsonArray = await x.json();
     jsonArray.forEach((item) => {
+        var divider = document.createElement('div');
+        divider.className = "tooltip";
         var paragraph = document.createElement('p');
         paragraph.textContent = item.name;
-        document.getElementById("score-list").appendChild(paragraph);
+        paragraph.className = "tooltip-paragraph";
+        divider.appendChild(paragraph);
+        var tooltip = document.createElement('span');
+        tooltip.textContent = item.tooltip;
+        tooltip.className = "tooltip-text";
+        paragraph.appendChild(tooltip);
+        document.getElementById("score-list").appendChild(divider);
     });
 }
+
+/*function copyID() {
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Copied the text: " + copyText.value);
+}*/
 
 //todo: take organ_scores.json, create a flex list with name : input score : * weight,
 //todo: hoverable tooltip, final score calculator, verdict "translator"
