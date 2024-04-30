b23b00d296aed12b518ce4fdbe51a709c8e21338
A5TR0spud <supercellstage@gmail.com>
2024-04-30 15:02:58 -0400

    more descriptive tooltip, id fix

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ async function createList() {
     let x = await fetch("./data/organ_scores.json");
     let jsonArray = await x.json();
     jsonArray.forEach((item) => {
+        //main div
         var divider = document.createElement('div');
         divider.className = "tooltip";
-        var paragraph = document.createElement('p');
-        paragraph.textContent = item.name;
-        paragraph.className = "tooltip-paragraph";
-        divider.appendChild(paragraph);
-        var tooltip = document.createElement('span');
-        tooltip.textContent = item.tooltip;
-        tooltip.className = "tooltip-text";
-        paragraph.appendChild(tooltip);
+        //main name
+        var name = document.createElement('p');
+        name.textContent = item.name;
+        name.className = "tooltip-paragraph";
+        divider.appendChild(name);
+        //tooltip main
+        var tooltip = document.createElement('div');
+        tooltip.className = "tooltip-container";
+        //name
+        var tooltip_name = document.createElement('p');
+        tooltip_name.textContent = item.name;
+        tooltip_name.className = "tooltip-name";
+        tooltip.appendChild(tooltip_name);
+        //description
+        var description = document.createElement('p');
+        description.textContent = item.tooltip;
+        description.className = "tooltip-description";
+        tooltip.appendChild(description);
+        //id
+        var description = document.createElement('p');
+        description.textContent = item.id;
+        description.className = "tooltip-id";
+        tooltip.appendChild(description);
+        //finalize
+        name.appendChild(tooltip);
         document.getElementById("score-list").appendChild(divider);
     });
 }
