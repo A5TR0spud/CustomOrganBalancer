abea3d4002f792c0e648180ab6a1214812bb35d8
A5TR0spud <supercellstage@gmail.com>
2024-04-30 20:13:24 -0400

    info box, useless input field

--- script.js
+++ script.js
@@ -6,41 +6,48 @@ async function createList() {
     jsonArray.forEach((item) => {
         //main div
         var divider = document.createElement('div');
-        divider.className = "tooltip";
+        divider.className = "score-list-item";
         //main name
         var name = document.createElement('p');
         name.textContent = item.name;
-        name.className = "tooltip-paragraph";
+        name.className = "score-title-paragraph";
         divider.appendChild(name);
-        //tooltip main
+
+        //tooltip hover able
+        var tooltip_info_box = document.createElement('p');
+        tooltip_info_box.textContent = "i";
+        tooltip_info_box.className = "info-box";
+        //tooltip container
         var tooltip = document.createElement('div');
         tooltip.className = "tooltip-container";
-        //name
+        //tooltip name
         var tooltip_name = document.createElement('p');
         tooltip_name.textContent = item.name;
         tooltip_name.className = "tooltip-name";
         tooltip.appendChild(tooltip_name);
-        //description
+        //tooltip description
         var description = document.createElement('p');
         description.textContent = item.tooltip;
         description.className = "tooltip-description";
         tooltip.appendChild(description);
-        //id
+        //tooltip id
         var description = document.createElement('p');
         description.textContent = item.id;
         description.className = "tooltip-id";
         tooltip.appendChild(description);
+        tooltip_info_box.appendChild(tooltip);
+        divider.appendChild(tooltip_info_box);
+
+        //input y the fool
+        var score_input = document.createElement('input');
+        score_input.value = "0.00";
+        score_input.className = "score-input";
+        score_input.step = "any";
+        divider.appendChild(score_input);
         //finalize
-        name.appendChild(tooltip);
         document.getElementById("score-list").appendChild(divider);
     });
 }
-
-/*function copyID() {
-  navigator.clipboard.writeText(copyText.value);
-
-  alert("Copied the text: " + copyText.value);
-}*/
 
 //todo: take organ_scores.json, create a flex list with name : input score : * weight,
 //todo: hoverable tooltip, final score calculator, verdict "translator"
